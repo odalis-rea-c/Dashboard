f05be8367e72024cbd5c3c024563da2ad9391026
odalis-rea-c <osxyrea@gmail.com>
2025-04-23 18:06:30 -0500

    Dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'miProyecto';
+
+  open = false;
+
+  toggleDropdown(){
+    this.open = !this.open
+    console.log(this.open)
+  }
 }
