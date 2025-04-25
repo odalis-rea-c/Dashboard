111efbcb2df370269552ca3224f3f01239b7ca1d
odalis-rea-c <osxyrea@gmail.com>
2025-04-25 17:24:02 -0500

    Otro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
+import { HeaderComponent } from './header/header.component';
 import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NgIf],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
